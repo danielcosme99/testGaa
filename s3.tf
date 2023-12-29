5afa7309dd6a7700944dbd2f81e87e5e07f7dc3a
Carlos Cosme <danielcosme99@gmail.com>
2023-12-28 19:24:41 -0500

    crear intancia test

---
+++ s3.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "ec2_instance" {
+  ami           = "ami-079db87dc4c10ac91"  # AMI de Linux
+  instance_type = "t2.micro"               # Tipo de instancia
+
+  vpc_security_group_ids = ["sg-0c4124408256d1902"]  # Grupo de seguridad
+  subnet_id              = "subnet-c07dc29f"          # Subred
+
+  iam_instance_profile = "SSM_Instance_Profile"  # Reemplaza con el nombre de tu IAM role
+
+  tags = {
+    Name = "temp-jroman1"
+  }
+}
